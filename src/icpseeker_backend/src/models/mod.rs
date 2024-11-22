36b45da0fd3e87ff82695d578ccdf1a6592da178
gracekarinn <gracekarina12@gmail.com>
2024-11-22 22:08:59 +0700

    feat: add chat and rate limit modules to models with relevant exports

--- src/icpseeker_backend/src/models/mod.rs
+++ src/icpseeker_backend/src/models/mod.rs
@@ -3,10 +3,13 @@ pub mod user;
 pub mod bank;
 pub mod education;
 pub mod cv;
+pub mod chat;
+pub mod rate_limit;
 
 pub use types::{StorageKey, FixedString, FixedContent};
-pub use types::{string_to_storage_key, string_to_fixed};
+pub use rate_limit::{UserAPIUsage, StableUserAPIUsage, RateLimitConfig};
 pub use user::UserProfile;
 pub use bank::BankInformation;
 pub use education::EducationRecord;
-pub use cv::CV;
+pub use cv::{CV, CVAnalysisStatus};
+pub use chat::{ChatMessage, ChatSession, ChatResponse, ChatHistoryResponse, StableChatSession, StableChatMessage};
