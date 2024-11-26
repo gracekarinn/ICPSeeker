bb8ecfd271362d6fc55aa15198f1ec0a3e821a3a
gracekarinn <gracekarina12@gmail.com>
2024-11-26 11:23:19 +0700

    feat: update Vite configuration to load environment variables and enhance build options

--- src/icpseeker_frontend/vite.config.js
+++ src/icpseeker_frontend/vite.config.js
@@ -1,78 +1,86 @@
 import { fileURLToPath, URL } from "url";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import environment from "vite-plugin-environment";
 import dotenv from "dotenv";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
-dotenv.config({ path: "../../.env" });
+dotenv.config();
 
-export default defineConfig({
-  build: {
-    emptyOutDir: true,
-    rollupOptions: {
-      external: [],
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis",
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    build: {
+      commonjsOptions: {
+        include: [/node_modules/],
+        transformMixedEsModules: true,
+      },
+      rollupOptions: {
+        external: [],
       },
     },
-    include: [
-      "@dfinity/auth-client",
-      "@dfinity/identity",
-      "@dfinity/agent",
-      "@dfinity/principal",
-    ],
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss(), autoprefixer()],
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: "globalThis",
+        },
+      },
+      include: [
+        "@dfinity/auth-client",
+        "@dfinity/identity",
+        "@dfinity/agent",
+        "@dfinity/principal",
+      ],
     },
-  },
-  server: {
-    host: "localhost",
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:4943",
-        changeOrigin: true,
-        secure: false,
+    css: {
+      postcss: {
+        plugins: [tailwindcss(), autoprefixer()],
       },
     },
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
+    server: {
+      host: "localhost",
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: "http://localhost:4943",
+          changeOrigin: true,
+        },
+      },
+      headers: {
+        "Cross-Origin-Opener-Policy": "same-origin",
+        "Cross-Origin-Embedder-Policy": "require-corp",
+      },
     },
-  },
-  plugins: [
-    react(),
-    environment("all", { prefix: "CANISTER_" }),
-    environment("all", { prefix: "DFX_" }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: "declarations",
-        replacement: fileURLToPath(new URL("../declarations", import.meta.url)),
-      },
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    ],
-  },
-  define: {
-    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-    "process.env.DFX_NETWORK": JSON.stringify(
-      process.env.DFX_NETWORK || "local"
-    ),
-    "process.env.CANISTER_ID_INTERNET_IDENTITY": JSON.stringify(
-      process.env.CANISTER_ID_INTERNET_IDENTITY
-    ),
-    global: {},
-  },
+    plugins: [react(), environment("all", { prefix: "VITE_" })],
+    resolve: {
+      alias: [
+        {
+          find: "declarations",
+          replacement: fileURLToPath(
+            new URL("./src/declarations", import.meta.url)
+          ),
+        },
+        {
+          find: "@",
+          replacement: fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      ],
+    },
+    define: {
+      global: "globalThis",
+      "process.env.DFX_NETWORK": JSON.stringify(env.DFX_NETWORK || "local"),
+      "process.env.CANISTER_ID_INTERNET_IDENTITY": JSON.stringify(
+        env.CANISTER_ID_INTERNET_IDENTITY
+      ),
+      "process.env.CANISTER_ID_ICPSEEKER_FRONTEND": JSON.stringify(
+        env.CANISTER_ID_ICPSEEKER_FRONTEND
+      ),
+      "process.env.CANISTER_ID_ICPSEEKER_BACKEND": JSON.stringify(
+        env.CANISTER_ID_ICPSEEKER_BACKEND
+      ),
+    },
+  };
 });
