b166a5638a38853a151d82146d9f55687c811b30
gracekarinn <gracekarina12@gmail.com>
2024-11-29 21:37:09 +0700

    feat: update Navbar component styles with new gradient colors and border

--- src/icpseeker_frontend/src/components/Navbar/Navbar.jsx
+++ src/icpseeker_frontend/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import { NavigationItems } from "./NavigationItems.jsx";
 import AuthButton from "../Auth/AuthButton";
-import Logo from "../../assets/logo.svg"; 
+import Logo from "../../assets/logo.svg";
 
 export default function Navbar() {
-    return (
-        <header className="sticky top-0 z-50 flex justify-between items-center w-full px-12 py-3 bg-gradient-to-r from-gradient-start to-gradient-end shadow-md border-b-4 border-purple-light">
-            <div className="flex-initial">
-                <img
-                    src={Logo}
-                    alt="ICP Seeker Logo"
-                    className="h-10 w-auto" 
-                />
-            </div>
-            <nav className="flex-grow flex justify-center text-white text-lg">
-                <NavigationItems />
-            </nav>
-            <div className="flex-initial">
-                <AuthButton />  
-            </div>
-        </header>
-    );
+  return (
+    <header className="sticky top-0 z-50 flex justify-between items-center w-full px-12 py-3 bg-gradient-to-r from-[#5F0EE7] to-[#350881] shadow-md border-b-4 border-[#EFE7FD]">
+      <div className="flex-initial">
+        <img src={Logo} alt="ICP Seeker Logo" className="h-10 w-auto" />
+      </div>
+      <nav className="flex-grow flex justify-center text-white text-lg">
+        <NavigationItems />
+      </nav>
+      <div className="flex-initial">
+        <AuthButton />
+      </div>
+    </header>
+  );
 }
